fc0e7085f535204d3c8213cdee57e62e216da5cb
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-05-18 17:07:57 -0500

    Agregando ruta para generar enlace corto

--- src/app.py
+++ src/app.py
@@ -1,34 +1,62 @@
-#Importando librerias
+# Importando librerias
 from flask import Flask, render_template, url_for, request, redirect, jsonify
 from flask_mysql_connector import MySQL
-import shourtuuid
+import shortuuid
 
-#Inicializando app
+# Inicializando app
 app = Flask(__name__)
 
-#endpoint
-endpoint = 'http://short.url'
+# endpoint
+endpoint = 'http://127.0.0.1:5500'
 
-#conexion a Mysql
+# conexion a Mysql
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DATABASE'] = 'db_enlaces_cortos'
 
-#Inicializando BD
+# Inicializando BD
 mysql = MySQL(app)
 
-#rutas
-@app.route('/', methods = ['GET'])
+# rutas
+
+
+@app.route('/', methods=['GET'])
 def inicio():
-     try:
-          return jsonify(respuesta = 'inicio')
-     except:
-          return jsonify(respuesta = 'Error de peticion'), 500
-     
+    try:
+        return jsonify(respuesta='inicio')
+    except:
+        return jsonify(respuesta='Error de peticion'), 500
 
 
-#Ejecutar app
+@app.route('/crear_enlace_corto', methods=['POST'])
+def crear_enlace_corto():
+    try:
+        if request.method == 'POST':
+            # capturando la url
+            url = request.form['url']
+            # cursor
+            cursor = mysql.connection.cursor()
+
+            enlace_corto = shortuuid.ShortUUID().random(length=7)
+
+            # Ingresamos en la base de datos  la url enviada
+            cursor.execute(
+                "INSERT INTO enlaces (irl, enlaces_cortos)VALUES (%s, %s)", (url, enlace_corto))
+
+            # Guardae cambios en la base de datos
+            mysql.connection.commit()
+
+            # Cerrar conexion
+            cursor.close()
+            nuevo_enlace = endpoint + '/'+enlace_corto
+
+        return jsonify(respuesta='inicio')
+    except:
+        return jsonify(respuesta='Error de peticion'), 500
+
+
+# Ejecutar app
 if __name__ == "__main__":
-     app.run(port=80, debug=True)
-    
+    app.run(port=5500, debug=True)
+    print(mysql)
